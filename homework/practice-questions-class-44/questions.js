c863db663e21c762831f0493acc7866e09bed5ac
vinccodes <vinccodes@gmail.com>
2021-05-10 23:46:03 -0400

    Added question 2 + TODOs.

--- homework/practice-questions-class-44/questions.js
+++ homework/practice-questions-class-44/questions.js
@@ -18,3 +18,21 @@ function multiplyArr(myList) {
 const theList = [2,4,6,8];
 
 console.log(multiplyArr(theList));
+
+/*
+QUESTION 2:
+You will be given an array of all the family members' ages,
+ in any order. The ages will be given in whole numbers, so a baby of 5 months, 
+ will have an ascribed 'age' of 0. 
+ Return a new array with 
+ [youngest age, oldest age, difference between the youngest and oldest age].
+ */
+
+ function familyReport(ages) {
+  // TODO:
+  // sort the ages into ascending order
+
+  // get the smallest age (first index)
+
+  // get the largest age (last index)
+}
