51ec2c0931209c36fe844e03df16cb496968615c
vinccodes <vinccodes@gmail.com>
2021-05-16 00:00:40 -0400

    Practie questions done 1,2.

--- homework/practice-questions-class-44/questions.js
+++ homework/practice-questions-class-44/questions.js
@@ -20,7 +20,8 @@ const theList = [2,4,6,8];
 console.log(multiplyArr(theList));
 
 /*
-QUESTION 2:
+Question 2
+
 You will be given an array of all the family members' ages,
  in any order. The ages will be given in whole numbers, so a baby of 5 months, 
  will have an ascribed 'age' of 0. 
@@ -29,24 +30,53 @@ You will be given an array of all the family members' ages,
  */
 
  function familyReport(ages) {
-  // TODO:
   // sort the ages into ascending order
-  for (let i = 0; i < ages.length; i++) {
-    for (let j = 0; j < ages.length; j++) {
-    
+  ages.sort(compareNumeric)
+  // get the smallest age (first index)
+  const smallest = ages[0];
+  // get the largest age (last index)
+  const largest = ages[ages.length - 1];
+  // difference between largest and smallest age
+  const difference = largest - smallest;
+
+  return [smallest, largest, difference]
+}  
+
+/* Question 3 
+
+Sum all the numbers of the array except the highest and the lowest element (the value, not the index!).
+Example:
+[ 6, 2, 1, 8, 10 ] => 16
+[ 1, 1, 11, 2, 3 ] => 6
+
+*/
+function sumLowHigh(arr) {
+  // sort the array
+  arr.sort(compareNumeric)
+  // get the first element
+  const lowest = arr[0];
+  // get the last element
+  const highest = arr[arr.length-1];
+
+  const exclude = [lowest, highest]
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    //TODO: if the current value is in exclude array, then skip
+    if (){
+      // skip 
+    }
+    else {
+      sum += arr[i];
     }
   }
 
-  // get the smallest age (first index)
-
-  // get the largest age (last index)
+  return sum;
 }
 
-// helper function used to swap two elements in bubble sort
-function swap(a,b) {
-  let temp = b;
-  b = a;
-  a = temp;
-  return [a,b]
+// helper function used in .sort() 
+function compareNumeric(a,b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
 }
-
