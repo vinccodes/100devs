75b0456765fb6c623a7f37f8f25fad4e986d5591
vinccodes <vinccodes@gmail.com>
2021-05-25 00:00:04 -0400

    Added string methods.

---
+++ homework/practice-questions-class-44/string-methods.js
@@ -0,0 +1,31 @@
+/* String Methods 
+
+charAt - returns string representation of char at specified index
+
+Examples
+
+const my_sentence = "100devs is cool";
+my_sentence.charAt(5); // v
+
+// returns empty string if index greater than .length-1
+
+my_sentence.charAt(my_sentence.length+1);
+
+
+
+charCodeAt
+concat
+includes
+indexOf
+match
+repeat
+replace
+search
+slice
+split
+substr
+toLowerCase
+toUpperCase
+trim
+
+*/
