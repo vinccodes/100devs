3a28ec5eeb97245ef5d622f94b10009ad3f1885c
vinccodes <vinccodes@gmail.com>
2021-05-27 23:07:03 -0400

    Finished some methods, working on String.match()

--- homework/practice-questions-class-44/string-methods.js
+++ homework/practice-questions-class-44/string-methods.js
@@ -2,7 +2,7 @@
 ===================
 charAt - returns string representation of char at specified index
 
-Example 1
+Example 1:
 const my_sentence = "100devs is cool";
 my_sentence.charAt(5); // v
 
@@ -27,7 +27,6 @@ Example 3: Non-integer passed as argument, defaults index to 0
 const sentence.charCodeAt("notAnInt"); // 49 
 
 
-
 =======
 concat - concats passed string(s) to the calling string returns new copy of string
 =======
@@ -48,16 +47,63 @@ const word = "world"
 const new_word = hello.concat(word); // helloworld
 
 ==========
-includes
+includes - check if one string found within another string (case-sensitive)
 ==========
 
-==========
-indexOf
-==========
+Example 1:
+
+const sentence = "this is a sentence";
+sentence.includes('this') // true
+
+Example 2
+const sentence = "this is a sentence";
+sentence.includes("not in sentence"); // false
+
+@param  position is optional, starts the search at that index
+Example 3: (searchString, positon) 
+
+const sentence = "javascript is very cool"
+sentence.includes('javascript', 1); // false
 
 ==========
-match
+indexOf - returns index within calling String object of FIRST OCCURENCE 
+          of passed in value, starting search at @fromIndex
+          RETURNS -1 if value not found
 ==========
+
+Example 1: 
+const sentence = "javascript is cool";
+console.log(sentence.indexOf("is")); // 11
+
+Example 2: indexOf(value, fromIndex)
+@fromIndex default is 0
+
+const sentence = "javascript is cool";
+console.log(sentence.indexOf("cool", 11)); // 14
+
+Example 3:
+const sentence = "javascript is cool";
+console.log(sentence.indexOf("is", 50)); // -1 because search starts at index 50; > str.length
+
+==========
+match - gets the result of matching string based on a regular expression (regex)
+
+      match(regexp)
+      @regexp   regular expression object
+
+      Nothing passed into match(), returns Array with empty "" ----> [""]
+==========
+
+Example 1:
+const APP_VERSION = v4.8
+const re = /v(\d+(\.\d)*)/i;
+console.log(APP_VERSION.match(re)) // ['v4.8', '4.8' '.8', index: 0, input: 'v4.8', groups: undefined]
+
+Example 2:
+
+
+Example 3: 
+
 
 ==========
 repeat
